02d896451a9e5bb3c7c471e7c573c56643236f51
RachaelKivuti <kivutingatha@gmail.com>
2024-05-20 11:14:55 +0300

    ios host platform channel

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,32 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    //platform channel
+    let flutterViewController: FlutterViewController = window?.rootViewController as | FlutterViewController
+    let deviceInfoChannel = FlutterMethodChannel(name: "platformchannel.companyname.com/deviceinfo", binaryMessenger: flutterViewController)
+
+    deviceInfoChannel.setMethodCallHandler({
+      (call:method == "getDeviceInfo") {
+        self.getDeviceInfo(result: result)
+      }
+      else {
+        result(FlutterMethodNotImplemented)
+      }
+    })
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  private func getDeviceInfo(result: FlutterResult) {
+    let device = UIDevice.current
+    var getDeviceInfo: String = "";
+    deviceInfo = "\nName: \(device.name)"
+    deviceInfo += "\nModel: \(device.model)"
+    deviceInfo += "\nSystem: \(device.systemName) \(device.systemVersion)"
+    deviceInfo += "\nProximity Monitoring Enabled: \(device.isProximityMonitoringEnabled)"
+    deviceInfo += "\nMultitasking Supported: \(device.isMultitaskingSupported)"
+    result(deviceInfo)
   }
 }
